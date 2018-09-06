b954dfcbf9b74c5fc49b7304f30d3b39ca13df7c
omarbenites <omarbenites@github.com>
2018-09-06 10:46:23 +0200

    server and ui session management

---
+++ R/server_session.R
@@ -0,0 +1,17 @@
+#'
+#' Session management for HIDAP-AGROFIMS
+#'
+#' @param input shinyserver input
+#' @param output shinyserver output
+#' @param session shinyserver session
+## @param dom target dom element name
+#' @param values reactive values
+#' @author Omar Benites Ivan Perez
+#' @export
+#'
+
+server_session <- function(input, output, session, values){
+
+
+
+}
---
+++ R/ui_session.R
@@ -0,0 +1,19 @@
+#' shiny UI session management in HIDAP-AGROFIMS
+#' 
+#' 
+#' @author Omar Benites Ivan Perez
+#' @param type type of ui element; default is a tab in a shiny dashboard
+#' @param title display title
+#' @param name a reference name
+#' @export
+
+ui_session <- function(type="tab",title="User session",name="open_session" ){
+
+
+
+}
+
+  
+  
+  
+  
